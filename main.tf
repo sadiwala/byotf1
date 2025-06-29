d053ece7c1b31fdf0964e916c735a2df0dbb7537
Parth Sadiwala <sadiwala1364@gmail.com>
2025-06-30 00:11:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "default" {
   project       = "google-mpf-982916601176"
-  name          = "byotf-bucket"
+  name          = "byotf-adadsdac-bucket"
   location      = "US"
   storage_class = "STANDARD"
 }
